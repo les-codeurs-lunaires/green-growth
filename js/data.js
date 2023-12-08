2e5e4e65bf6d3ce2ff6d6fce2d3726ab38befb97
Micky <mickycomptepro@gmail.com>
2023-12-08 04:18:57 +0100

    script.js  - creation script data.js  - creation data

---
+++ js/data.js
@@ -0,0 +1,16 @@
+const data = [
+        {
+            "question":"question1",
+            "correction": [
+                "correction1",
+                "correction2",
+                "correction3",
+                "correction4",
+            ],
+            "reponse": [
+                "reponse1",
+                "reponse2",
+                "reponse3",
+                "reponse4",
+            ]
+        }]
