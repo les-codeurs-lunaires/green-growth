7c88d3997e64c3de44a4ad664e256f34126d637c
Micky <mickycomptepro@gmail.com>
2023-12-08 06:37:45 +0100

    data.js index.html transitions.js correction de pas mal de bugs

--- js/data.js
+++ js/data.js
@@ -14,3 +14,14 @@ const data = [
                 "reponse4",
             ]
         }]
+
+const data2 = [
+    "Un monde idéal ?",
+    "Imaginez un monde parfait, " +
+    "où tout va toujours bien, dans lequel la météo est parfaitement prévisible, " +
+    "les catastrophes naturelles, anticipées, sont devenu inoffensives.",
+    "Cependant, ce monde n’est pas le nôtre. En réalité, " +
+    "le réchauffement climatique s’aggrave chaque jour et les efforts pour le retarder ne sont pas bien répartis… " +
+    "Préjugé, procrastination, intérêts personnel, mauvaises informations, les causes sont nombreuses. " +
+    "Parviendrez-vous à démêler le vrai du faux et, à votre échelle, sauver le monde ?"
+]
