954710046390d7ec4b59205b71984d1c8730b4c4
Micky Mari <micky.mari@etu.u-bordeaux.fr>
2025-05-08 01:30:47 +0200

    switch vps @untypequicode

--- js/db-header.js
+++ js/db-header.js
@@ -1,21 +1,19 @@
 const db_header = [
-    {
-        text: "Tester mes connaissances",
-        url: "qcm",
-    },
-    {
-        text: "S'informer",
-        url: "informer",
-    }
+  {
+    text: "Tester mes connaissances",
+    url: "qcm",
+  },
+  {
+    text: "S'informer",
+    url: "informer",
+  },
 ];
 
 const logo = {
-    type: "imagetext",
-    content: [
-        "images/favicon.svg",
-        "Green Growth"],
-    link: "https://les-codeurs-lunaires.github.io/green-growth/",
-    index: "Accueil",
-}
+  type: "imagetext",
+  content: ["images/favicon.svg", "Green Growth"],
+  link: "https://green-growth.untypequicode.fr/",
+  index: "Accueil",
+};
 
-const switch_mode = "false"
+const switch_mode = "false";
